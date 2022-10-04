f08861d87f3327b552dc32cb489defde05f77302
qwerewqwerew <qwerew0@gmail.com>
2022-10-04 11:59:26 +0900

    상품업로드 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import 'antd/dist/antd.css';
+import "./App.css";
+import { UploadOutlined } from "@ant-design/icons";
 import MainPage from "./components/MainPage";
 import ProductPage from "./components/ProductPage";
 import UploadPage from "./components/UploadPage";
-import "./App.css";
+import { Button } from "antd";
 
 const App = () => {
-	return (
-		<>
-			<div id="header">
-				<div id="header-area">
-					<img src="images/icons/logo.png" alt="logo" />
-				</div>
-			</div>
+	let navigate=useNavigate();
 
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route path="/upload" element={<UploadPage />} />
-				<Route path="product/:id" element={<ProductPage />} />
-			</Routes>
+  return (
+    <>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="images/icons/logo.png" alt="logo" />
+          </Link>
+          <Button size="large" icon={<UploadOutlined />} onClick={()=>{navigate('/upload')}}>상품업로드</Button>
+        </div>
+      </div>
 
-			<div id="footer">
-				<a href="#">회사소개</a>
-				<a href="#">이용약관</a>
-				<a href="#">통신판매업:123-1234</a>
-				<a href="#">사업자등록번호:456-4567</a>
-				<a href="#">개인정보...</a>
-			</div>
-		</>
-	);
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="product/:id" element={<ProductPage />} />
+      </Routes>
+
+      <div id="footer">
+        <a href="#">회사소개</a>
+        <a href="#">이용약관</a>
+        <a href="#">통신판매업:123-1234</a>
+        <a href="#">사업자등록번호:456-4567</a>
+        <a href="#">개인정보...</a>
+      </div>
+    </>
+  );
 };
 export default App;
--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,4 +1,81 @@
+import { Form, Divider, InputNumber, Button, Upload, Input } from "antd";
+import "./UploadPage.css";
+
 const UploadPage = () => {
-	return <div>UploadPage</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <div id="body">
+      <div id="upload-container">
+        <Form name="uploadForm" onFinish={onFinish}>
+          <Form.Item name="upload">
+            <div id="upload-img">
+              <img src="/images/icons/camera.png" />
+              <span>상품 이미지를 업로드 해주세요</span>
+            </div>
+          </Form.Item>
+          <Divider />
+          <Form.Item
+            label={<span className="upload-label">상품명</span>}
+            name="product-name"
+            rules={[
+              { required: true, message: "상품명은 필수 입력 사항입니다." },
+            ]}
+          >
+            <Input
+              className="upload-name"
+              placeholder="상품명을 입력해주세요"
+            />
+          </Form.Item>
+          {/* row */}
+          <Form.Item
+            label={<span className="upload-label">판매자명</span>}
+            name="product-seller"
+            rules={[
+              { required: true, message: "판매자명은 필수 입력 사항입니다." },
+            ]}
+          >
+            <Input
+              className="upload-name"
+              placeholder="판매자명을 입력해주세요"
+            />
+          </Form.Item>
+          {/* row */}
+          <Form.Item
+            label={<span className="upload-label">판매가</span>}
+            name="product-price"
+            rules={[
+              { required: true, message: "판매가는 필수 입력 사항입니다." },
+            ]}
+          >
+            <InputNumber className="upload-name" defaultValue={0} min={0} />
+          </Form.Item>
+          {/* row */}
+          <Form.Item
+            label={<span className="upload-label">상품설명</span>}
+            name="product-desc"
+            rules={[
+              { required: true, message: "상품설명은 필수 입력 사항입니다." },
+            ]}
+          >
+            <Input.TextArea
+              className="upload-name"
+              placeholder="상품설명을 입력하세요"
+              showCount
+              maxLength={300}
+            />
+          </Form.Item>
+          <Divider />
+          <Form.Item>
+            <Button id="submit-button" size="large" htmlType="submit">
+              상품등록하기
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 export default UploadPage;
