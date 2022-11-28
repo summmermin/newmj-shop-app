35fd2e119764d5e04479eb067daf7bc038747083
summmermin <doniminiblog@gmail.com>
2022-11-28 16:27:03 +0900

    서버수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 //constants.js
-export const API_URL= process.env.NODE_ENV === 'production'? "https://newmj-shop-server.herokuapp.com" : "http://localhost:8080";
+export const API_URL= process.env.NODE_ENV === 'production'? "https://newmj-shop-app.herokuapp.com" : "http://localhost:8080";
